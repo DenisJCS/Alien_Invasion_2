a01df70a50fec17d1e904917e310ba602c76587f
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2025-03-08 17:09:13 -0500

    Add files via upload

---
+++ ship.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Ship():
+    """Class for ruling the ship"""
+    def __init__(self, ai_game):
+        """Initialize ship and sets it start position"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Loading ship image and get rectangle
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        # Start each new ship at the bottom of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Draw ship in present postition"""
+        self.screen.blit(self.image, self.rect)
+
