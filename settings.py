c1ca37077e0a5578ba044fcb0d4c6d3f2d4b4e26
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2025-03-11 23:26:19 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,5 +5,8 @@ class Settings():
         """Initiated game settings"""
         #Parameters of screen
         self.screen_width = 1200
-        self.scree_height = 800
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 1.5
